3d66668ae0a56c905e8f8df43c4280c302c70b38
Jasper <jasperfue@live.de>
2025-06-09 22:01:30 +0200

    add .env file with secrets

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 app = FastAPI()
 
+class Settings(BaseSettings):
+    top_energy_username: str
+    top_energy_password: str
+    top_energy_host: str
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
+
+configuration = tepyapi.Configuration(
+    username = settings.top_energy_username,
+    password = settings.top_energy_password,
+    host = settings.top_energy_host
+)
 
 @app.get("/")
 async def root():
