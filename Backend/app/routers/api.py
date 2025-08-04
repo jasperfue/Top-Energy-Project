ebc7d2e1f679824ccb8cb39a255946c30f91ef79
Jasper <jasperfue@live.de>
2025-08-04 17:16:40 +0200

    Add Backend-Route to Load Project

--- Backend/app/routers/api.py
+++ Backend/app/routers/api.py
@@ -1,7 +1,12 @@
+import tepyapi
 from fastapi import APIRouter, HTTPException
 from typing import List
 from pathlib import Path
 import asyncio
+
+from tepyapi import models, apis
+
+from app.core.config import get_tepy_configuration
 
 router = APIRouter()
 
@@ -36,3 +41,26 @@ async def get_projects():
         raise HTTPException(status_code=403, detail="Keine Berechtigung auf das Projects-Verzeichnis")
     except Exception:
         raise HTTPException(status_code=500, detail="Unbekannter Fehler beim Einlesen der Projekte")
+
+@router.get("/projects/{project_name}")
+async def get_project(project_name: str):
+    project_path = PROJECTS_DIR / project_name
+    if not project_path.exists():
+        raise HTTPException(status_code=404, detail=f"Projekt '{project_name}' nicht gefunden")
+    if not project_path.is_file():
+        raise HTTPException(status_code=400, detail=f"'{project_name}' ist kein gültiges Projekt")
+    configuration = get_tepy_configuration()
+    try:
+        with tepyapi.ApiClient(configuration) as api_client:
+            process_api = apis.EfProcessManagementApi(api_client)
+            print("Load project ... ", end = '', flush = True)
+            print(str(project_path))
+            prj_data = models.ApiProjectData(
+                str(project_path),
+                sourceType = "file",
+                readonly = True
+            )
+            api_response = process_api.load_project(project_data=prj_data)
+            return api_response
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Fehler beim Lesen des Projekts: {str(e)}")
