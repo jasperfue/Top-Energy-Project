7d1d4d814f642a2abe5b26e5079dae89ae832690
Jasper <jasperfue@live.de>
2025-12-28 14:33:43 +0100

    change favicon

--- webapp/src/routes/__root.tsx
+++ webapp/src/routes/__root.tsx
@@ -43,7 +43,10 @@ export const Route = createRootRouteWithContext<{
 		links: [
 			{ rel: "stylesheet", href: stylesCss },
 			{ rel: "manifest", href: "/site.webmanifest", color: "#ffffff" },
-			{ rel: "icon", href: "/favicon.ico" },
+			{
+				rel: "icon",
+				href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>",
+			},
 		],
 	}),
 	errorComponent: ({ error }) => ErrorPage(error),
