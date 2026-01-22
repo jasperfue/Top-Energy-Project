19d0a63326014931da8edfdcbb7759defc9d9f7b
Jasper <jasperfue@live.de>
2026-01-22 12:28:07 +0100

    use session to store all data and send to airtable only at the end

--- webapp/src/lib/useUserSession.ts
+++ webapp/src/lib/useUserSession.ts
@@ -1,7 +1,53 @@
 import { useSession } from "@tanstack/react-start/server";
 
 type userSession = {
-	recId?: string;
+	"Teilnehmer ID"?: string;
+	Zielgruppe?: boolean;
+	afftech_q1?: number;
+	afftech_q2?: number;
+	afftech_q3?: number;
+	afftech_q4?: number;
+	afftech_q5?: number;
+	afftech_q6?: number;
+	afftech_q7?: number;
+	afftech_q8?: number;
+	afftech_q9?: number;
+	Studienvariante?: "Chat" | "Dashboard";
+	Startzeit?: string;
+	Endzeit?: string;
+	Nachrichten_Anzahl?: number;
+	trust_comp_1?: number;
+	trust_comp_2?: number;
+	trust_comp_3?: number;
+	trust_comp_4?: number;
+	trust_ben_1?: number;
+	trust_ben_2?: number;
+	trust_ben_3?: number;
+	trust_int_1?: number;
+	trust_int_2?: number;
+	trust_int_3?: number;
+	trust_int_4?: number;
+	understanding_q1?: number;
+	understanding_q2?: number;
+	understanding_q3?: string;
+	understanding_q4?: string;
+	ueq_1?: number;
+	ueq_2_swapped?: number;
+	ueq_3?: number;
+	ueq_4_swapped?: number;
+	ueq_5?: number;
+	ueq_6_swapped?: number;
+	ueq_7?: number;
+	ueq_8_swapped?: number;
+	intention_q1?: number;
+	intention_q2?: number;
+	intention_q3?: number;
+	age?: number;
+	gender?: string;
+	occupation_role?: string;
+	domain_background?: string;
+	investment_experience?: string;
+	feedback?: string;
 };
 
 export const useUserSession = () =>
